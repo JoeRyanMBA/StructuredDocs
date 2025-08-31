7bc5798ac7a33a47ced824e1c5298884f4721b7d
JoeRyanMBA <joe@joe-ryan.mba>
2025-08-31 01:19:36 +0000

    Remove frontend/dist from git tracking and add to .gitignore
    - Removed all frontend/dist files from git tracking
    - Added frontend/dist/ to .gitignore
    - Frontend build files should be generated, not tracked in git
    - This allows proper rebuilding with updated CSS changes

--- frontend/dist/assets/IncorporateFeedback-laTCc8B_.js
+++
@@ -1,2 +0,0 @@
-import{_ as r,c as t,a as o,o as a}from"./index-CyQaDqYh.js";const n={name:"Incorporate Feedback"};function c(s,e,p,l,d,i){return a(),t("div",null,[...e[0]||(e[0]=[o("h2",null,"Incorporate Feedback",-1),o("p",null,"Use this tool to incorporate reviewer feedback.",-1)])])}const u=r(n,[["render",c]]);export{u as default};
-//# sourceMappingURL=IncorporateFeedback-laTCc8B_.js.map
