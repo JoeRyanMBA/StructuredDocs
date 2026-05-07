fa187c7671fec1e1f3377b26365a0df1a23ce857
JoeRyanMBA <joe@joe-ryan.mba>
2026-05-07 20:29:52 +0000

    refactor: Update list node handling in RichTextEditor to use specific tagName for improved accuracy

--- frontend/src/components/RichTextEditor.vue
+++ frontend/src/components/RichTextEditor.vue
@@ -473,7 +473,7 @@ export default {
             cursor += 1
             continue
           }
-          if (node instanceof HTMLElement && ['UL', 'OL'].includes(node.tagName)) {
+          if (node instanceof HTMLElement && node.tagName === tagName) {
             listNodes.push(node)
             cursor += 1
             continue
